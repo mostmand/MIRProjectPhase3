9af00cc5e7ec7a5b0286aa8007075e76205b37d0
Mohammad Hossein Mostmand <mostmand.mh@gmail.com>
2019-01-04 19:36:01 +0330

    Fixed some problems

--- crawler/crawler.py
+++
@@ -1,7 +0,0 @@
-from scrapy import cmdline
-import datetime
-
-
-def crawl(n):
-    path = 'josns/' + str(datetime.datetime.now()) + '/'
-    cmdline.execute("scrapy crawl digikala -a n={0} -path={1}".format(n, path).split())
--- crawler/main.py
+++ crawler/main.py
@@ -1,6 +1,15 @@
+import datetime
+import os
+from scrapy import cmdline
 import indexing.indexer
 import page_ranking.page_ranker
-from crawler import crawler
+
+
+def crawl(n):
+    path = 'jsons/' + str(datetime.datetime.now()) + '/'
+    if not os.path.exists(path):
+        os.makedirs(path)
+    cmdline.execute(['scrapy', 'crawl', 'digikala', '-a', 'n={0}'.format(n), '-a', 'path={0}'.format(path)])
 
 
 def get_input_from_user():
@@ -12,13 +21,10 @@ def get_input_from_user():
         print('4.Calculate PageRank and index it into elastic')
         print('5.Search')
         print('0.Exit')
-        try:
-            inp = int(input())
-            if inp == 0:
-                break
-            yield inp
-        except:
-            print('Your input is not recognized')
+        inp = int(input())
+        if inp == 0:
+            break
+        yield inp
 
 
 jsons_path = 'jsons'
@@ -28,15 +34,19 @@ index_name = 'test-index3'
 for inp in get_input_from_user():
     print(inp)
     if inp == 1:
-        n = int(input('Please enter number of all pages to be crawled\n'))
-        crawler.crawl(n)
+        print('Please enter number of all pages to be crawled')
+        n = int(input())
+        crawl(n)
     elif inp == 2:
         indexing.indexer.remove_index(elastic_address, index_name)
     elif inp == 3:
         indexing.indexer.index(jsons_path, elastic_address, index_name)
     elif inp == 4:
-        alpha = float(input('Please enter damping factor (alpha)\n'))
+        print('Please enter damping factor (alpha)')
+        alpha = float(input())
         teleportation_rate = 0.1
         page_ranking.page_ranker.PageRanker(elastic_address, index_name).rank_pages(alpha, teleportation_rate)
     elif inp == 5:
         pass
+    else:
+        print('Your input is not recognized')
